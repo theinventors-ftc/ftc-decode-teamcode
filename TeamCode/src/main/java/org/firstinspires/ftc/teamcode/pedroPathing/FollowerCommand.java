79895c83ef5ffed5311297cffd8ed14a913ee577
GrBrogremmer09 <georafpr@gmail.com>
2026-02-16 20:59:12 +0200

    12 Classified Better Alliance 12

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/FollowerCommand.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/FollowerCommand.java
@@ -8,15 +8,21 @@ public class FollowerCommand extends CommandBase {
     private Follower follower;
     private PathChain path;
     private double velocity;
+    private boolean roll;
 
     public FollowerCommand(Follower follower, PathChain path) {
         this(follower, path, 1.0);
     }
 
     public FollowerCommand(Follower follower, PathChain path, double velocity) {
+        this(follower, path, velocity, false);
+    }
+
+    public FollowerCommand(Follower follower, PathChain path, double velocity, boolean roll) {
         this.follower = follower;
         this.path = path;
         this.velocity = velocity;
+        this.roll = roll;
     }
 
     public void initialize() {
@@ -31,6 +37,12 @@ public class FollowerCommand extends CommandBase {
 
     public boolean isFinished() {
 //        return follower.atParametricEnd();
+        if (roll) {
+            if (follower.getCurrentTValue() >= 0.75) {
+                follower.pausePathFollowing();
+                return true;
+            }
+        }
         return !follower.isBusy();
     }
 }
